0556455dfafe18a8d86001c62e0eb0fb51fb71a1
Bartłomiej Zimny <b.zimny@ideo.pl>
2018-04-14 15:02:37 +0200

    poprawienie działania notyfikacji zmiana struktury bazy

--- src/components/bus-notification/index.js
+++ src/components/bus-notification/index.js
@@ -1,9 +1,41 @@
 import React, {Component} from "react";
-import {View, TouchableOpacity, Text, StyleSheet} from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  PushNotificationIOS,
+  Linking,
+  DeviceEventEmitter
+} from "react-native";
 import {Icon} from "native-base";
 import Moment from "moment";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import PushNotification from "react-native-push-notification";
+import AppLink from "react-native-app-link";
+import PushNotificationAndroid from "react-native-push-notification";
 class BusNotification extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentWillMount = () => {
+    PushNotificationAndroid.registerNotificationActions(["Yes", "No"]);
+    DeviceEventEmitter.addListener("notificationActionReceived", action => this.notificationAction(action));
+    PushNotification.configure({
+      onRegister: function (token) {
+        console.log("TOKEN:", token);
+      },
+      onNotification: function (notification) {
+        console.log("NOTIFICATION:", notification);
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      }
+    });
+  };
+
+  componentWillUnmount = action => {
+    DeviceEventEmitter.removeListener("notificationActionReceived", action => this.notificationAction(action));
+  };
   render() {
     return <View style={styles.itemContener}>
       <TouchableOpacity onPress={() => this.onDelete()}>
@@ -36,9 +68,11 @@ class BusNotification extends Component {
         alignItems: "center"
       }}
         onPress={this.props.openModal}>
-        <View style={{width:45}}>
-        <Icon name={this.props.transport} style={styles.colorStyle}/>
-</View>
+        <View style={{
+          width: 45
+        }}>
+          <Icon name={this.props.transport} style={styles.colorStyle}/>
+        </View>
         <Text style={styles.colorStyle}>
           {Moment(this.props.time).format("HH:mm")}
         </Text>
@@ -54,6 +88,21 @@ class BusNotification extends Component {
       .update({
         active: !this.props.active
       });
+    if (!this.props.active) {
+      PushNotification.localNotificationSchedule({
+        id: this.props.id, autoCancel: false, color: "#2196F3", // (optional) default: system default
+        title: "Transport Notification", // (optional)
+        message: this.props.direction + " " + Moment(this.props.time).format("HH:mm") + " " + this.props.transport, // (required) // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+        repeatType: "day", // (Android only) Repeating interval. Could be one of `week`, `day`, `hour`, `minute, `time`. If specified as time, it should be accompanied by one more parameter 'repeatTime` which should the number of milliseconds between each interval
+        date: Moment().set({
+          hours: Moment(this.props.time).hours(),
+          minutes: Moment(this.props.time).minutes(),
+          seconds: 0
+        }).toDate()
+      });
+    } else {
+      PushNotification.cancelLocalNotifications({id: this.props.id});
+    }
   }
   onDelete() {
     this
@@ -61,7 +110,18 @@ class BusNotification extends Component {
       .doc
       .ref
       .delete();
+    PushNotification.cancelLocalNotifications({id: this.props.id});
   }
+  notificationAction = (action) => {
+    console.log("Notification action received: " + action);
+    const info = JSON.parse(action.dataJSON);
+    if (info.action == "Yes") {
+      console.log("yes");
+    } else if (info.action == "No") {
+      console.log("no");
+    }
+  };
+
 }
 var styles = StyleSheet.create({
   itemContener: {
--- src/scene/Home.js
+++ src/scene/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {
   Platform,
   StyleSheet,
@@ -7,7 +7,10 @@ import {
   TouchableOpacity,
   Modal,
   TextInput,
-  Alert
+  Alert,
+  PushNotificationIOS,
+  Linking,
+  DeviceEventEmitter
 } from "react-native";
 
 import {
@@ -27,7 +30,7 @@ import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import LinearGradient from "react-native-linear-gradient";
 import DateTimePicker from "react-native-modal-datetime-picker";
 import Moment from "moment";
-import { Actions } from "react-native-router-flux";
+import {Actions} from "react-native-router-flux";
 
 import PickerIcon from "@components/picker-icon";
 import Fab from "@components/fab";
@@ -40,10 +43,9 @@ import Head from "@components/head";
 import firebase from "react-native-firebase";
 
 import BusNotification from "@components/bus-notification";
-import PushNotify from "@components/push-notify";
-
 import PushNotification from "react-native-push-notification";
-
+import AppLink from "react-native-app-link";
+import PushNotificationAndroid from "react-native-push-notification";
 export default class Home extends Component {
   constructor(props) {
     super(props);
@@ -51,7 +53,9 @@ export default class Home extends Component {
     this.ref = firebase
       .firestore()
       .collection("notifications")
-      .doc(this.props.userId)
+      .where('uid', '==', this.props.userId);
+    this.collection = firebase
+      .firestore()
       .collection("notifications");
 
     this.state = {
@@ -59,20 +63,15 @@ export default class Home extends Component {
         {
           name: "bus",
           active: true
-        },
-        {
+        }, {
           name: "train"
-        },
-        {
+        }, {
           name: "car"
-        },
-        {
+        }, {
           name: "boat"
-        },
-        {
+        }, {
           name: "jet"
-        },
-        {
+        }, {
           name: "subway"
         }
       ],
@@ -81,26 +80,43 @@ export default class Home extends Component {
       pickerVisible: false,
       time: new Date(),
       select: null,
-      city: "",
+      direction: "",
       index: -1
     };
   }
   componentDidMount() {
-    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+    this.unsubscribe = this
+      .ref
+      .onSnapshot(this.onCollectionUpdate);
   }
 
-  componentWillUnmount() {
+  componentWillUnmount = action => {
     this.unsubscribe();
-  }
+    DeviceEventEmitter.removeListener("notificationActionReceived", action => this.notificationAction(action));
+  };
 
   onCollectionUpdate = querySnapshot => {
     const busSchedule = [];
     querySnapshot.forEach(doc => {
-      const { time, direction, transport, active } = doc.data();
-      busSchedule.push({ time, direction, transport, active, doc });
+      const {time, direction, transport, active} = doc.data();
+      busSchedule.push({time, direction, transport, active, doc});
     });
     console.log(querySnapshot);
-    this.setState({ busSchedule });
+    this.setState({busSchedule});
+  };
+
+  componentWillMount = () => {
+    PushNotificationAndroid.registerNotificationActions(["Yes", "No"]);
+    DeviceEventEmitter.addListener("notificationActionReceived", action => this.notificationAction(action));
+    PushNotification.configure({
+      onRegister: function (token) {
+        console.log("TOKEN:", token);
+      },
+      onNotification: function (notification) {
+        console.log("NOTIFICATION:", notification);
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      }
+    });
   };
 
   render() {
@@ -110,144 +126,133 @@ export default class Home extends Component {
           right={true}
           icon={"person"}
           onPress={() => Actions.Person()}
-          text="Transport Notification"
-        />
+          text="Transport Notification"/>
         <View style={styles.fullStyles}>
           <FlatList
-            data={this.state.busSchedule
-              .sort(this.compareDate)
-              .sort(this.compareNotification)}
+            data={this
+            .state
+            .busSchedule
+            .sort(this.compareDate)
+            .sort(this.compareNotification)}
             contentContainerStyle={styles.flatListStyle}
-            renderItem={({ item }) => (
-              <BusNotification
-                openModal={() => this.selectTransport(item)}
-                {...item}
-              />
-            )}
-          />
+            renderItem={({item}) => (<BusNotification openModal={() => this.selectTransport(item)} {...item}/>)}/>
           <Modal
             visible={this.state.modalVisible}
             animationType="slide"
             transparent={false}
-            onRequestClose={() =>
-              this.setState({
-                modalVisible: !this.state.modalVisible
-              })
-            }
-            animationType={"slide"}
-          >
+            onRequestClose={() => this.setState({
+            modalVisible: !this.state.modalVisible
+          })}
+            animationType={"slide"}>
             <View style={styles.modalContener}>
               <Head
                 left={true}
                 leftIcon={"close"}
-                leftPress={() =>
-                  this.setState({
-                    modalVisible: !this.state.modalVisible
-                  })
-                }
+                leftPress={() => this.setState({
+                modalVisible: !this.state.modalVisible
+              })}
                 text={"Add Transport"}
                 right={true}
                 icon={"save"}
-                onPress={() => this.saveTransport()}
-              />
-              <LinearGradient
-                colors={["#2196F3", "#E3F"]}
-                style={styles.contentContener}
-              >
+                onPress={() => this.saveTransport()}/>
+              <LinearGradient colors={["#2196F3", "#E3F"]} style={styles.contentContener}>
                 <PickerIcon
                   icons={this.state.transports}
                   select={this.state.select}
-                  select={item => this.selectPicker(item)}
-                />
+                  select={item => this.selectPicker(item)}/>
                 <View style={styles.viewTime}>
                   <TouchableOpacity
-                    onPress={() =>
-                      this.setState({
-                        pickerVisible: !this.state.pickerVisible
-                      })
-                    }
-                  >
+                    onPress={() => this.setState({
+                    pickerVisible: !this.state.pickerVisible
+                  })}>
                     <Text style={styles.textTime}>
                       {Moment(this.state.time).format("HH:mm")}
                     </Text>
                   </TouchableOpacity>
-                  <Icon name="time" />
+                  <Icon name="time"/>
                 </View>
                 <Input
                   underlineColorAndroid="transparent"
                   placeholder="Enter City"
-                  value={this.state.city}
-                  onChangeText={text => this.setState({ city: text })}
-                />
+                  value={this.state.direction}
+                  onChangeText={text => this.setState({direction: text})}/>
               </LinearGradient>
             </View>
-            <PushNotify />
           </Modal>
           <DateTimePicker
             date={this.state.time}
             isVisible={this.state.pickerVisible}
-            onConfirm={time => this.setState({ time })}
-            onCancel={() =>
-              this.setState({
-                pickerVisible: !this.state.pickerVisible
-              })
-            }
-            mode={"time"}
-          />
+            onConfirm={time => this.setState({time})}
+            onCancel={() => this.setState({
+            pickerVisible: !this.state.pickerVisible
+          })}
+            mode={"time"}/>
           <Fab
-            onPress={() =>
-              this.setState({
-                modalVisible: !this.state.modalVisible
-              })
-            }
-            icon={"md-add"}
-          />
+            onPress={() => this.setState({
+            modalVisible: !this.state.modalVisible
+          })}
+            icon={"md-add"}/>
         </View>
       </Container>
     );
   }
   selectPicker(value) {
     var tab = [];
-    this.state.transports.forEach(item => {
-      var obj = item;
-      if (value != obj) obj.active = null;
-      else {
-        obj.active = true;
-      }
-      tab.push(obj);
-    });
-    this.setState({ transports: tab });
+    this
+      .state
+      .transports
+      .forEach(item => {
+        var obj = item;
+        if (value != obj) 
+          obj.active = null;
+        else {
+          obj.active = true;
+        }
+        tab.push(obj);
+      });
+    this.setState({transports: tab});
   }
   compareNotification(a, b) {
-    if (a.active && !b.active) return -1;
-    if (!a.active && b.active) return 1;
+    if (a.active && !b.active) 
+      return -1;
+    if (!a.active && b.active) 
+      return 1;
     return 0;
   }
   compareDate(a, b) {
-    if (a.time < b.time) return -1;
-    if (a.time > b.time) return 1;
+    if (a.time < b.time) 
+      return -1;
+    if (a.time > b.time) 
+      return 1;
     return 0;
   }
   toggleNotification(value) {
     var tab = [];
-    this.state.busSchedule.forEach(item => {
-      if (value == item) {
-        item.active = !item.active;
-      }
-      tab.push(item);
-    });
-    this.setState({ busSchedule: tab });
+    this
+      .state
+      .busSchedule
+      .forEach(item => {
+        if (value == item) {
+          item.active = !item.active;
+        }
+        tab.push(item);
+      });
+    this.setState({busSchedule: tab});
   }
   selectTransport = item => {
     var tab = [];
-    this.state.transports.forEach(value => {
-      var obj = value;
-      if (item.transport !== obj.name) obj.active = null;
-      else {
-        obj.active = true;
-      }
-      tab.push(obj);
-    });
+    this
+      .state
+      .transports
+      .forEach(value => {
+        var obj = value;
+        if (item.transport !== obj.name) 
+          obj.active = null;
+        else {
+          obj.active = true;
+        }
+        tab.push(obj);
+      });
     this.setState({
       select: item,
       transports: tab,
@@ -257,56 +262,59 @@ export default class Home extends Component {
     });
   };
   saveTransport = () => {
-    if (this.state.city.length==0){
-      alert("Miasto jest puste.");
+    if (this.state.direction.length == 0) {
+      alert("City is empty.");
       return;
     }
     var transport = "";
     if (this.state.select == null) {
       var obj = {
         time: this.state.time,
-        direction: this.state.city
+        direction: this.state.direction,
+        uid: this.props.userId,
+        id: Moment().unix()
       };
-      this.state.transports.forEach(element => {
-        if (element.active == true) {
-          obj.transport = element.name;
-        }
-      });
-      this.ref.add(obj);
+      this
+        .state
+        .transports
+        .forEach(element => {
+          if (element.active == true) {
+            obj.transport = element.name;
+          }
+        });
+      this
+        .collection
+        .add(obj);
     }
     if (this.state.select != null) {
-      this.state.transports.forEach(element => {
-        if (element.active == true) {
-          transport = element.name;
-        }
-      });
-      this.state.select.doc.ref.update({
-        time: this.state.time,
-        direction: this.state.city,
-        transport
-      });
-      PushNotification.localNotificationSchedule({
-        id :Moment().unix(),
-        autoCancel: false, // (optional) default: true
-        largeIcon: "ic_launcher", // (optional) default: "ic_launcher"
-        smallIcon: "ic_notification", // (optional) default: "ic_notification" with fallback for "ic_launcher"
-        bigText: "Go to " + transport + " to " + this.state.city + " .", // (optional) default: "message" prop
-        color: "#2196F3", // (optional) default: system default
-        vibrate: true, // (optional) default: true
-        vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
-        userInfo:{id:Moment().unix()},
-        /* iOS and Android properties */
-        title: "Transport Notification", // (optional)
-        message:
-          this.state.city +
-          " " +
-          Moment(this.state.time).format("HH:mm") +
-          " " +
-          transport, // (required)
-        soundName: "default", // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
-        repeatType: "day", // (Android only) Repeating interval. Could be one of `week`, `day`, `hour`, `minute, `time`. If specified as time, it should be accompanied by one more parameter 'repeatTime` which should the number of milliseconds between each interval
-        date: dateTime // in 60 secs
-      });
+      this
+        .state
+        .transports
+        .forEach(element => {
+          if (element.active == true) {
+            transport = element.name;
+          }
+        });
+      this
+        .state
+        .select
+        .doc
+        .ref
+        .update({time: this.state.time, direction: this.state.direction, transport});
+      if (this.state.select.active) {
+        PushNotification.cancelLocalNotifications({id: this.state.select.id});
+        PushNotification.localNotificationSchedule({
+          id: this.state.select.id, autoCancel: false, color: "#2196F3", // (optional) default: system default
+          title: "Transport Notification", // (optional)
+          message: this.state.direction + " " + Moment(this.state.time).format("HH:mm") + " " + transport,
+          repeatType: "day", // (Android only) Repeating interval. Could be one of `week`, `day`, `hour`, `minute, `time`. If specified as time, it should be accompanied by one more parameter 'repeatTime` which should the number of milliseconds between each interval
+          date: Moment().set({
+            hours: Moment(this.state.time).hours(),
+            minutes: Moment(this.state.time).minutes(),
+            seconds: 0
+          }).toDate()
+        });
+      }
     }
     var dateTime = new Date(Date.now() + 60 * 1000);
 
