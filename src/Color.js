d12714a18590046d5b8b9208131f5cc74914c880
Bartłomiej Zimny <b.zimny@ideo.pl>
2018-04-28 14:09:48 +0200

    poprawienie Kolorów

--- src/Color.js
+++ src/Color.js
@@ -1,12 +1,12 @@
 export default {
-  primaryColor = "#2196F3",
-  secondaryColor = "#1a78c2",
-  accentColor = "#E3F",
-  textColor = "#000",
-  placeholderColor = "#b8b8b8",
-  disabledColor = "#e9e9e9",
-  backgroundColor = "#fff",
-  google = "#756d6d",
-  facebook = "#3b5998"
+  primaryColor : "#2196F3",
+  secondaryColor : "#1a78c2",
+  accentColor : "#E3F",
+  textColor : "#000",
+  placeholderColor : "#b8b8b8",
+  disabledColor : "#e9e9e9",
+  backgroundColor : "#fff",
+  google : "#756d6d",
+  facebook : "#3b5998"
 };
 
